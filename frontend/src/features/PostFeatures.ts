0f0e82f8936e8e59724843ffde615857608941b4
unknown <emad.m.alnagar@gmail.com>
2024-01-24 21:30:43 +0200

    Add new post service

---
+++ frontend/src/features/PostFeatures.ts
@@ -0,0 +1,67 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = 'http://localhost:5000/api/posts';
+
+export interface Post {
+  _id?: object;
+  title: string;
+  slug: string;
+  author: object,
+  description: string;
+  image: string;
+  likes?: object[];
+  likesCount?: number;
+  comments?: string[];
+}
+
+interface PostState {
+  posts: Post[],
+  post: Post | null,
+  isLoading: boolean,
+  error: object | null
+}
+
+const initialState: PostState = {
+  posts: [],
+  post: null,
+  isLoading: false,
+  error: null
+}
+
+// Create a new 
+export const NewPost: any = createAsyncThunk("posts/new", async (post: any, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(`${url}/new`, {
+      title: post.title,
+      author: post.author,
+      description: post.description,
+      image: post.image,
+    });
+    return response.data;
+  } catch (error: any) {
+    return rejectWithValue(error.message);
+  }
+});
+
+const postSlice = createSlice({
+  name: 'post',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(NewPost.pending, (state) => {
+      state.isLoading = false;
+    })
+    .addCase(NewPost.fulfilled, (state, action) => {
+      state.isLoading = true;
+      state.posts.push(action.payload);
+    })
+    .addCase(NewPost.rejected, (state, action) => {
+      state.isLoading = true;
+      state.error = action.error;
+    })
+  }
+});
+
+export default postSlice.reducer;
