bc2069c09f7ef4e14728c7620efef8fbbfb8b6e1
unknown <emad.m.alnagar@gmail.com>
2024-01-31 23:18:10 +0200

    Category form

---
+++ frontend/src/components/RowForm.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+const NewContainerForm = () => {
+  return (
+    <Fragment>
+      <form>
+        <input type="text" placeholder="category title" />
+        <button>+</button>
+      </form>
+    </Fragment>
+  )
+}
