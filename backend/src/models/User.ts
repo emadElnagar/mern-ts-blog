ff332b9fabba423c31d7d5a26fe1efb73a8457e6
unknown <emad.m.alnagar@gmail.com>
2024-02-16 23:36:50 +0200

    User role

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -5,8 +5,7 @@ interface User {
   lastName: string;
   email: string;
   password: string;
-  isAdmin: boolean;
-  isModerator: boolean;
+  role: string;
   image?: string;
 }
 
@@ -15,8 +14,11 @@ const userSchema = new Schema<User>({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-  isModerator: { type: Boolean, default: false },
+  role: {
+    type: String,
+    enum: ['admin', 'moderator', 'user'],
+    default: 'user'
+  },
   image: String,
 }, {
   timestamps: true
