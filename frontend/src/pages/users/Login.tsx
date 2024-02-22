c26dd17c0e0f90b321b9fb63cad08bf20b46f986
unknown <emad.m.alnagar@gmail.com>
2024-02-22 17:16:10 +0200

    navigate if user is logged in

--- frontend/src/pages/users/Login.tsx
+++ frontend/src/pages/users/Login.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { Login } from "../../features/UserFeatures";
 
 const LoginPage = () => {
+  const { user } = useSelector((state: any) => state.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch(Login({ email, password }));
+  }
+  if (user) {
+    navigate('/');
   }
   return (
     <div className="container">
