47cb5065f76a57927df6bd7af67ae5efd2523f55
unknown <emad.m.alnagar@gmail.com>
2024-02-24 16:50:34 +0200

    rich text editor

--- frontend/src/pages/posts/NewPost.tsx
+++ frontend/src/pages/posts/NewPost.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 const NewPostPage = () => {
+  const [value, setValue] = useState('');
   return (
     <div className="container">
       <h1 className="text-center text-capitalize heading">new post</h1>
       <form>
         <input type="text" placeholder="title" />
-        <textarea placeholder="post content"></textarea>
+        <ReactQuill theme="snow" value={value} onChange={setValue} />;
         <input type="file" accept="*/images" />
         <input type="submit" value="submit" />
       </form>
