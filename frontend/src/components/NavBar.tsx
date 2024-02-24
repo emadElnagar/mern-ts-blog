4704f793299fb4f57eedc99f4479a2e76beec3a3
unknown <emad.m.alnagar@gmail.com>
2024-02-24 07:47:58 +0200

    Create dropdown

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,19 @@
 import { IoIosSearch } from "react-icons/io";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isActive, setIsActive] = useState(false);
+  const { user } = useSelector((state: any) => state.user);
+  const handleDropDown = () => {
+    if (isActive === false) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }
   return (
     <nav>
       <div className="logo">
@@ -16,7 +28,22 @@ const NavBar = () => {
         </form>
       </div>
       <div className="account">
-        <Link to="/users/login">login</Link>
+        {
+          user === null ? (
+            <Link to="/users/login">login</Link>
+          ) : (
+            <div className="dropdown">
+              <button className="dropdown-button" onClick={handleDropDown}>
+                <IoMdArrowDropdown className={`${isActive === false ? '' : 'rotate'}`} />
+                { user.firstName }
+              </button>
+              <ul className={`${isActive === false ? 'dropdown-content d-none' : 'dropdown-content'}`}>
+                <li className="dropdown-item">profile</li>
+                <li className="dropdown-item">logout</li>
+              </ul>
+            </div>
+          )
+        }
       </div>
     </nav>
   )
