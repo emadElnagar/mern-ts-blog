aff2f11266f16f0593888bb0df9692cf449a0c70
unknown <emad.m.alnagar@gmail.com>
2024-03-03 21:25:42 +0200

    Start admin dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import NewPostPage from './pages/posts/NewPost';
 import NotFoundPage from './pages/NotFound';
 import { useSelector } from 'react-redux';
 import Categories from './pages/categories/Categories';
+import Dashboard from './pages/admin';
 
 function App() {
   const { user } = useSelector((state: any) => state.user);
@@ -28,6 +29,12 @@ function App() {
           <Fragment>
             <Route path='/posts/new' element={<NewPostPage />} />
             <Route path='/categories/new' element={<Categories />} />
+          </Fragment>
+        }
+        {
+          user !== null && user.role === 'admin' &&
+          <Fragment>
+            <Route path='/admin' element={<Dashboard />} />
           </Fragment>
         }
         <Route path='*' element={<NotFoundPage />} />
