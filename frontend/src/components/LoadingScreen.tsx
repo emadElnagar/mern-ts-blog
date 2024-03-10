3a2186885a773ded83b74b733115332abb5ff782
unknown <emad.m.alnagar@gmail.com>
2024-03-10 22:25:10 +0200

    Loading screen

---
+++ frontend/src/components/LoadingScreen.tsx
@@ -0,0 +1,11 @@
+import RingLoader  from "react-spinner";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loader">
+      <RingLoader />
+    </div>
+  )
+}
+
+export default LoadingScreen;
