6d6af56d90748c02d31e8594c8c850a63f4c92b1
unknown <emad.m.alnagar@gmail.com>
2024-03-13 15:13:05 +0200

    Admin dashboard main page

--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -1,7 +1,23 @@
+import { IoIosAdd } from "react-icons/io";
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
   return (
     <div className="container">
-      <h1>Admin Dashboard</h1>
+      <h1 className="heading text-center">MAGALA</h1>
+      <ul>
+        <li className="row">
+          <span><Link to='/admin/users'>users</Link></span>
+        </li>
+        <li className="row">
+          <span><Link to='/admin/categories'>categories</Link></span>
+          <button><Link to='/admin/categories/new'><IoIosAdd /> Add</Link></button>
+        </li>
+        <li className="row">
+          <span><Link to='/admin/posts'>posts</Link></span>
+          <button><Link to='/admin/posts/new'><IoIosAdd /> Add</Link></button>
+        </li>
+      </ul>
     </div>
   )
 }
