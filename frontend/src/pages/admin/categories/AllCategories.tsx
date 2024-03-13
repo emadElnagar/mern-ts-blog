e6f65d5e6ccbb33b150000053355ab81068819ad
unknown <emad.m.alnagar@gmail.com>
2024-03-13 15:52:29 +0200

    Get All ctegories implementation

--- frontend/src/pages/admin/categories/AllCategories.tsx
+++ frontend/src/pages/admin/categories/AllCategories.tsx
@@ -1,20 +1,32 @@
+import { Key, useEffect } from "react";
 import { IoPencil } from "react-icons/io5";
 import { MdDelete } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { GetAllCategories } from "../../../features/CategoryFeatures";
 
 
 const AllCategories = () => {
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state: any) => state.category);
+  useEffect(() => {
+    dispatch(GetAllCategories());
+  }, [dispatch]);
   return (
     <div className="container">
       <h1 className="heading text-center">all categories</h1>
-      <ul>
-        <li className="row">
-          <span>cars</span>
-          <div className="control">
-            <button><MdDelete /> delete</button>
-            <button><IoPencil /> update</button>
-          </div>
-        </li>
-      </ul>
+        {
+          categories.map((category: { _id: Key, title: string }) => (
+            <ul key={ category._id }>
+              <li className="row">
+                <span>{ category.title }</span>
+                <div className="control">
+                  <button><MdDelete /> delete</button>
+                  <button><IoPencil /> update</button>
+                </div>
+              </li>
+            </ul>
+          ))
+        }
     </div>
   )
 }
