afb15a26ab10bb25d818ffc6081836e937486371
unknown <emad.m.alnagar@gmail.com>
2024-03-15 16:14:01 +0200

    Handle delete user

--- frontend/src/pages/admin/users/UsersList.tsx
+++ frontend/src/pages/admin/users/UsersList.tsx
@@ -1,15 +1,37 @@
 import { Key, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { GetAllUsers } from "../../../features/UserFeatures";
+import { DeleteUser, GetAllUsers } from "../../../features/UserFeatures";
 import { IoPencil } from "react-icons/io5";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const UsersList = () => {
   const dispatch = useDispatch();
   const { users } = useSelector((state: any) => state.user);
   useEffect(() => {
     dispatch(GetAllUsers());
-  }, [dispatch])
+  }, [dispatch]);
+  const handleDelete = (id: Key) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(DeleteUser(id));
+        dispatch(GetAllUsers());
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  }
   return (
     <div className="container">
       <h1 className="heading text-center">users list</h1>
@@ -21,7 +43,7 @@ const UsersList = () => {
               <span>{ user.role }</span>
               <div className="control">
                 <button><IoPencil /> update</button>
-                <button className="delete"><MdDelete /> delete</button>
+                <button onClick={() => handleDelete(user._id)} className="delete"><MdDelete /> delete</button>
               </div>
             </li>
           </ul>
