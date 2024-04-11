2f316519aa9ff8de73a42e58c7cded9b3b04e9d7
unknown <emad.m.alnagar@gmail.com>
2024-04-11 22:55:18 +0200

    Image preview while uploading

--- frontend/src/components/PostForm.tsx
+++ frontend/src/components/PostForm.tsx
@@ -4,9 +4,11 @@ import 'react-quill/dist/quill.snow.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetAllCategories } from '../features/CategoryFeatures';
 import { NewPost } from '../features/PostFeatures';
+import { useNavigate } from 'react-router-dom';
 
 const PostForm = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [content, setContent] = useState('');
   const [image, setImage] = useState<File | undefined>();
   const [title, setTitle] = useState('');
@@ -31,24 +33,33 @@ const PostForm = () => {
     formData.append('content', content);
     formData.append('category', category);
     formData.append('author', user._id);
-    dispatch(NewPost(formData));
+    dispatch(NewPost(formData)).then(() => {
+      navigate('/');
+    });
   }
   return (
     <form className="colum-form" onSubmit={handleSubmit}>
-        <input type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)} />
-        <ReactQuill theme="snow" value={content} onChange={setContent} />
-        <select onChange={(e) => setCategory(e.target.value)}>
-          {
-            categories.map((category: { _id: Key; title: string; }) => (
-              <option key={ category._id } value={`${ category._id }`}>
-                { category.title }
-              </option>
-            ))
-          }
-        </select>
-        <input type="file" accept="*/images" onChange={handleImage} />
-        <input type="submit" value="submit" />
-      </form>
+      <input type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)} />
+      {
+        image && (
+          <div className='img-preview'>
+            <img src={URL.createObjectURL(image)} alt='Problem showing your iamge' />
+          </div>
+        )
+      }
+      <ReactQuill theme="snow" value={content} onChange={setContent} />
+      <select onChange={(e) => setCategory(e.target.value)}>
+        {
+          categories.map((category: { _id: Key; title: string; }) => (
+            <option key={ category._id } value={`${ category._id }`}>
+              { category.title }
+            </option>
+          ))
+        }
+      </select>
+      <input type="file" accept="*/images" onChange={handleImage} />
+      <input type="submit" value="submit" />
+    </form>
   )
 }
 
