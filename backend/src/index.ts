22e8e675344c1982963c3306a2232e2757b1af1c
unknown <emad.m.alnagar@gmail.com>
2024-04-12 09:04:36 +0200

    Handle change user image

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/blog');
 
--- frontend/src/pages/users/profile.tsx
+++ frontend/src/pages/users/profile.tsx
@@ -1,17 +1,41 @@
-import { Fragment, useEffect } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { Helmet } from "react-helmet"
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
-import { GetSingleUser } from "../../features/UserFeatures";
+import { ChagneImage, GetSingleUser } from "../../features/UserFeatures";
 import LoadingScreen from "../../components/LoadingScreen";
+import { FaCamera } from "react-icons/fa";
+import { AiOutlineCheck } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 
 const UserProfile = () => {
   const { id } = useParams();
   const { user, profile, isloading } = useSelector((state: any) => state.user);
+  const [image, setImage] = useState<File | undefined>();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(GetSingleUser(id));
   }, [dispatch, id]);
+  // handle upload image
+  const handleImage = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement & {
+      files: FileList;
+    }
+    setImage(target.files[0]);
+  }
+  // handle submit
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (typeof image === 'undefined') return;
+    const formData = new FormData();
+    formData.append('profile', image);
+    dispatch(ChagneImage({
+      id: user._id,
+      formData
+    }));
+    setImage(undefined);
+    dispatch(GetSingleUser(id));
+  }
   return (
     <Fragment>
       <Helmet>
@@ -24,9 +48,32 @@ const UserProfile = () => {
           <div className="img-container">
             <div className="image">
               <img
-                src={`${profile.image ? `${profile.image}` : `${process.env.PUBLIC_URL + '/images/user-avatar.png'}`}`} 
+                src={`${
+                  image ? URL.createObjectURL(image)
+                  : profile.image 
+                  ? `http://localhost:5000/${profile.image}` 
+                  : `${process.env.PUBLIC_URL + '/images/user-avatar.png'}`}`
+                } 
                 alt="user avatar"
               />
+              {
+                user && user._id === profile._id &&
+                <form onSubmit={handleSubmit}>
+                  {
+                    image ? (
+                      <div className="buttons">
+                        <button type="submit"><AiOutlineCheck /></button>
+                        <button onClick={() => setImage(undefined)}><AiOutlineClose /></button>
+                      </div>
+                    ) : (
+                      <>
+                        <input type="file" id="img" accept="*/images" onChange={handleImage} />
+                        <label htmlFor="img"><FaCamera className="icon" /></label>
+                      </>
+                    )
+                  }
+                </form>
+              }
             </div>
             <h1 className="heading text-center">{profile.firstName} {profile.lastName}</h1>
           </div>
