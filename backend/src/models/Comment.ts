034bf59eabb55ed98bccdddcf1379b9f68eb9c86
unknown <emad.m.alnagar@gmail.com>
2024-04-16 08:15:34 +0200

    Adding replies to comment

--- backend/src/models/Comment.ts
+++ backend/src/models/Comment.ts
@@ -2,14 +2,28 @@ import { Schema, model } from 'mongoose';
 
 interface Comment {
   post: object;
-  user: object;
+  author: object;
   body: string;
+  replies: [
+    {
+      author: object,
+      createdAt: Date,
+      body: string
+    }
+  ]
 }
 
 const commentSchema = new Schema<Comment>({
   post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   body: { type: String, required: true },
+  replies: [
+    {
+      author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+      createdAt: new Date().getTime(),
+      body: { type: String, required: true }
+    }
+  ]
 }, {
   timestamps: true
 });
