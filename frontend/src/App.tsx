7c7d4b6256066780e9509f334ef5736ccbacc1f0
unknown <emad.m.alnagar@gmail.com>
2024-04-26 21:23:45 +0200

    Start update post page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import { Fragment } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/HomePage';
-import LoginPage from './pages/users/Login';
-import RegisterPage from './pages/users/Register';
-import NavBar from './components/NavBar';
-import NewPostPage from './pages/posts/NewPost';
-import NotFoundPage from './pages/NotFound';
-import { useSelector } from 'react-redux';
-import Categories from './pages/categories/Categories';
-import Dashboard from './pages/admin';
-import CategoryNew from './pages/admin/categories/NewCategory';
-import AllCategories from './pages/admin/categories/AllCategories';
-import UsersList from './pages/admin/users/UsersList';
-import UserProfile from './pages/users/Profile';
-import SinglePost from './pages/posts/SinglePost';
-import AllPosts from './pages/admin/posts/AllPosts';
+import { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import LoginPage from "./pages/users/Login";
+import RegisterPage from "./pages/users/Register";
+import NavBar from "./components/NavBar";
+import NewPostPage from "./pages/posts/NewPost";
+import NotFoundPage from "./pages/NotFound";
+import { useSelector } from "react-redux";
+import Categories from "./pages/categories/Categories";
+import Dashboard from "./pages/admin";
+import CategoryNew from "./pages/admin/categories/NewCategory";
+import AllCategories from "./pages/admin/categories/AllCategories";
+import UsersList from "./pages/admin/users/UsersList";
+import UserProfile from "./pages/users/Profile";
+import SinglePost from "./pages/posts/SinglePost";
+import AllPosts from "./pages/admin/posts/AllPosts";
 
 function App() {
   const { user } = useSelector((state: any) => state.user);
@@ -27,30 +27,29 @@ function App() {
         </header>
       </div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/users/login' element={<LoginPage />} />
-        <Route path='/users/register' element={<RegisterPage />} />
-        <Route path='/users/profile/:id' element={<UserProfile />} />
-        <Route path='/posts/:slug' element={<SinglePost />} />
-        {
-          user !== null && user.role !== 'user' &&
+        <Route path="/" element={<Home />} />
+        <Route path="/users/login" element={<LoginPage />} />
+        <Route path="/users/register" element={<RegisterPage />} />
+        <Route path="/users/profile/:id" element={<UserProfile />} />
+        <Route path="/posts/:slug" element={<SinglePost />} />
+        {user !== null && user.role !== "user" && (
           <Fragment>
-            <Route path='/posts/new' element={<NewPostPage />} />
-            <Route path='/categories/new' element={<Categories />} />
+            <Route path="/posts/new" element={<NewPostPage />} />
+            <Route path="/categories/new" element={<Categories />} />
           </Fragment>
-        }
-        {
-          user !== null && user.role === 'admin' &&
+        )}
+        {user !== null && user.role === "admin" && (
           <Fragment>
-            <Route path='/admin' element={<Dashboard />} />
-            <Route path='/admin/categories' element={<AllCategories />} />
-            <Route path='/admin/categories/new' element={<CategoryNew />} />
-            <Route path='/admin/posts' element={<AllPosts />} />
-            <Route path='/admin/posts/new' element={<NewPostPage />} />
-            <Route path='/admin/users' element={<UsersList />} />
+            <Route path="/admin" element={<Dashboard />} />
+            <Route path="/admin/categories" element={<AllCategories />} />
+            <Route path="/admin/categories/new" element={<CategoryNew />} />
+            <Route path="/admin/posts" element={<AllPosts />} />
+            <Route path="/admin/posts/new" element={<NewPostPage />} />
+            <Route path="/admin/posts/:slug/update" element={<NewPostPage />} />
+            <Route path="/admin/users" element={<UsersList />} />
           </Fragment>
-        }
-        <Route path='*' element={<NotFoundPage />} />
+        )}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/admin/posts/UpdatePost.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { Helmet } from "react-helmet";
+
+const UpdatePost = () => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Magagla-post-update</title>
+      </Helmet>
+      <div className="container">
+        <h1 className="heading">update post</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default UpdatePost;
