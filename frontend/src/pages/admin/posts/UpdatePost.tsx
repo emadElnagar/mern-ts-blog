9fb83db72d1462742f144e621c4db8e23f7f2fe8
unknown <emad.m.alnagar@gmail.com>
2024-06-09 21:11:56 +0200

    Update post form

--- frontend/src/pages/admin/posts/UpdatePost.tsx
+++ frontend/src/pages/admin/posts/UpdatePost.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import { Helmet } from "react-helmet";
+import { MdCloudUpload } from "react-icons/md";
+import ReactQuill from "react-quill";
 
 const UpdatePost = () => {
   return (
@@ -9,6 +11,18 @@ const UpdatePost = () => {
       </Helmet>
       <div className="container">
         <h1 className="heading">update post</h1>
+        <form className="colum-form">
+          <input type="text" placeholder="title" />
+          <ReactQuill theme="snow" />
+          <select>
+            <option>category</option>
+          </select>
+          <input type="file" id="image-upload" accept="*/images" />
+          <label htmlFor="image-upload" className="upload-label">
+            <MdCloudUpload /> upload image
+          </label>
+          <input type="submit" value="Post" />
+        </form>
       </div>
     </Fragment>
   );
