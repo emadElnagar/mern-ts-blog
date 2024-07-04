2987a9a0df3468e132173399d70ccbb39ba3480e
unknown <emad.m.alnagar@gmail.com>
2024-07-04 13:35:58 +0200

    Limit request for security

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json({ limti: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(express.static("uploads"));
 
