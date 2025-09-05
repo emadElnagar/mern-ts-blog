500d87f2a94799f85d059961271f5a424d81bcd4
unknown <emad.m.alnagar@gmail.com>
2025-09-05 20:51:53 +0200

    Improve category controllers and routes

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -1,75 +1,90 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Response } from "express";
 import Category from "../models/Category";
+import { AuthenticatedRequest } from "../types/authTypes";
 
 // Create a new category
-export const newCategory: RequestHandler = async (req, res) => {
-  const foundCategoryTitle = await Category.findOne({ title: req.body.title });
-  if (foundCategoryTitle) {
-    return res
-      .status(401)
-      .json({ message: "This title already exists, Try another name" });
-  }
-  interface categoryType {
-    title: string;
-    author: object;
-  }
-  const category = new Category<categoryType>({
-    title: req.body.title,
-    author: req.body.author,
-  });
-  category
-    .save()
-    .then((category) => {
-      res.status(200).json({
-        category,
-      });
-    })
-    .catch((err) => {
-      res.status(401).json({
-        message: err.message,
-      });
+export const newCategory = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const foundCategoryTitle = await Category.findOne({
+      title: req.body.title,
     });
+    if (foundCategoryTitle) {
+      return res
+        .status(401)
+        .json({ message: "This title already exists, Try another name" });
+    }
+    interface categoryType {
+      title: string;
+      author: string;
+    }
+    const category = new Category<categoryType>({
+      title: req.body.title,
+      author: req.user._id,
+    });
+    await category.save();
+    res.status(200).json({ message: "Category created successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Get all categories
 export const getAllCategories: RequestHandler = async (_req, res) => {
   try {
     const categories = await Category.find();
-    res.send(categories);
-  } catch (error) {
-    res.send(error);
+    if (!categories) {
+      return res.status(404).json({ message: "No categories found" });
+    }
+    res.status(200).json(categories);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
   }
 };
 
 // Update category
-export const updateCategory: RequestHandler = async (req, res) => {
-  const newCategory = {
-    title: req.body.title,
-  };
-  Category.updateOne({ _id: req.params.id }, { $set: newCategory })
-    .then((_result) => {
-      res.status(200).json({
-        message: "Category updated successfully",
-      });
-    })
-    .catch((error) => {
-      res.status(401).json({
-        message: "Error" + error.message,
-      });
+export const updateCategory = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const newCategory = {
+      title: req.body.title,
+    };
+    const foundCategory = await Category.findOne({
+      title: req.body.title,
+      _id: { $ne: req.params.id },
     });
+    if (foundCategory) {
+      return res
+        .status(401)
+        .json({ message: "This category already exists, Try another name" });
+    }
+    if (
+      !req.body.title ||
+      typeof req.body.title !== "string" ||
+      !req.body.title.trim()
+    ) {
+      return res.status(400).json({ message: "Invalid category title" });
+    }
+    await Category.updateOne({ _id: req.params.id }, { $set: newCategory });
+    res.status(200).json({
+      message: "Category updated successfully",
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Delete category
-export const deleteCategory: RequestHandler = (req, res) => {
-  Category.deleteOne({ _id: req.params.id })
-    .then((_result) => {
-      res.status(200).json({
-        message: "Category deleted successfully",
-      });
-    })
-    .catch((error) => {
-      res.status(401).json({
-        message: "Error" + error.message,
-      });
-    });
+export const deleteCategory = (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const category = Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    Category.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- backend/src/routes/categoryRoutes.ts
+++ backend/src/routes/categoryRoutes.ts
@@ -1,23 +1,41 @@
 import { Router } from "express";
-import { 
-  deleteCategory, 
-  getAllCategories, 
-  newCategory, 
-  updateCategory 
+import {
+  deleteCategory,
+  getAllCategories,
+  newCategory,
+  updateCategory,
 } from "../controllers/categoryController";
+import { isAdmin, isAuth } from "../middlewares/auth";
+import { RequestHandler } from "express-serve-static-core";
+import { AuthenticatedRequest } from "../types/authTypes";
 
 const categoryRouter = Router();
 
 // New category
-categoryRouter.post('/new', newCategory);
+categoryRouter.post(
+  "/new",
+  isAuth as RequestHandler,
+  isAdmin as RequestHandler,
+  (req, res) => newCategory(req as AuthenticatedRequest, res)
+);
 
 // Get all categories
-categoryRouter.get('/all', getAllCategories);
+categoryRouter.get("/all", getAllCategories);
 
 // Update category
-categoryRouter.put('/:id/update', updateCategory);
+categoryRouter.put(
+  "/:id/update",
+  isAuth as RequestHandler,
+  isAdmin as RequestHandler,
+  (req, res) => updateCategory(req as AuthenticatedRequest, res)
+);
 
 // Delete category
-categoryRouter.delete('/:id/delete', deleteCategory);
+categoryRouter.delete(
+  "/:id/delete",
+  isAuth as RequestHandler,
+  isAdmin as RequestHandler,
+  (req, res) => deleteCategory(req as AuthenticatedRequest, res)
+);
 
 export default categoryRouter;
