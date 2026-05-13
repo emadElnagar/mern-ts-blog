3e54455bcde81bc02ce3b4eb14e2c7dd05c6fe81
unknown <emad.m.alnagar@gmail.com>
2026-05-13 22:20:56 +0200

    Update single post page style

--- frontend/src/pages/posts/SinglePost.tsx
+++ frontend/src/pages/posts/SinglePost.tsx
@@ -70,99 +70,115 @@ const SinglePost = () => {
       ) : (
         post && (
           <div className="container">
-            <div className="single-post">
-              <div className="post-img-container">
-                <img
-                  src={`${BASE_URL}/${post.image}`}
-                  alt="It's problem showing images"
-                />
-                <span className="author">
-                  Author:{" "}
-                  <Link to={`/users/profile/${post.author._id}`}>
-                    {post.author.firstName} {post.author.lastName}
-                  </Link>
-                </span>
-                <br />
-                <span className="date">
-                  Created at: {moment(post.createdAt).format("MMMM Do YYYY")}
-                </span>
-              </div>
-              <h1 className="post-title">{post.title}</h1>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(post.content),
-                }}
-              ></div>
-            </div>
-            {/* Post actions (like, comment) */}
-            <PostActions
-              likesCount={post.likes.length}
-              isLiked={user ? post.likes.includes(user._id) : false}
-              commentsCount={comments.length}
-              onLikePost={() => handlePostLike(post._id)}
-              postSlug={post.slug}
-            />
-            <div className="comments">
-              {user ? (
-                <form onSubmit={handleSubmit}>
-                  <input
-                    type="text"
-                    placeholder="Leave a comment"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                  />
-                  <button type="submit" style={{ display: "none" }}></button>
-                </form>
-              ) : (
-                <Link to={`/users/login?next=/posts/${post.slug}`}>
-                  <form>
-                    <input
-                      className="comment-input"
-                      type="text"
-                      placeholder="login to leave a comment"
+            <div className="page-layout">
+              <div>
+                {/* Single post */}
+                <div className="single-post">
+                  <div className="post-img-container">
+                    <img
+                      src={`${BASE_URL}/${post.image}`}
+                      alt="It's problem showing images"
                     />
-                  </form>
-                </Link>
-              )}
-              <section className="post-comments">
-                <h3>Comments ({comments.length})</h3>
+                    <span className="author">
+                      Author:{" "}
+                      <Link to={`/users/profile/${post.author._id}`}>
+                        {post.author.firstName} {post.author.lastName}
+                      </Link>
+                    </span>
+                    <br />
+                    <span className="date">
+                      Created at:{" "}
+                      {moment(post.createdAt).format("MMMM Do YYYY")}
+                    </span>
+                  </div>
+                  <h1 className="post-title">{post.title}</h1>
+                  <div
+                    className="post-content"
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(post.content),
+                    }}
+                  ></div>
+                </div>
 
-                {comments?.map((comment: Comment) => (
-                  <CommentItem
-                    key={comment._id}
-                    comment={comment}
-                    user={user}
-                    onDelete={handleDelete}
-                    onUpdate={handleUpdate}
-                    onLike={handleLike}
-                    onReply={createReply}
-                    postSlug={post.slug}
-                  />
-                ))}
-              </section>
-            </div>
-            {similarPosts?.length > 0 && (
-              <div className="similar-posts">
-                <h2 className="text-center">You may also like</h2>
-                <div className="posts-container">
-                  {similarPosts.map((post: any) => (
-                    <Link key={post._id} to={`/posts/${post.slug}`}>
-                      <Card
-                        _id={post._id}
-                        title={post.title}
-                        content={post.content}
-                        category={post.category?.title}
-                        image={post.image}
-                        author={`${post.author?.firstName} ${post.author?.lastName}`}
-                        authorImage={post.author?.image}
-                        commentsCount={post.commentsCount}
-                        date={new Date(post.createdAt).toLocaleDateString()}
+                {/* Post actions (like, comment) */}
+                <PostActions
+                  likesCount={post.likes.length}
+                  isLiked={user ? post.likes.includes(user._id) : false}
+                  commentsCount={comments.length}
+                  onLikePost={() => handlePostLike(post._id)}
+                  postSlug={post.slug}
+                />
+
+                {/* Comments section */}
+                <div className="comments">
+                  {user ? (
+                    <form onSubmit={handleSubmit}>
+                      <input
+                        type="text"
+                        placeholder="Leave a comment"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
                       />
+                      <button
+                        type="submit"
+                        style={{ display: "none" }}
+                      ></button>
+                    </form>
+                  ) : (
+                    <Link to={`/users/login?next=/posts/${post.slug}`}>
+                      <form>
+                        <input
+                          className="comment-input"
+                          type="text"
+                          placeholder="login to leave a comment"
+                        />
+                      </form>
                     </Link>
-                  ))}
+                  )}
+                  <section className="post-comments">
+                    <h3>Comments ({comments.length})</h3>
+
+                    {comments?.map((comment: Comment) => (
+                      <CommentItem
+                        key={comment._id}
+                        comment={comment}
+                        user={user}
+                        onDelete={handleDelete}
+                        onUpdate={handleUpdate}
+                        onLike={handleLike}
+                        onReply={createReply}
+                        postSlug={post.slug}
+                      />
+                    ))}
+                  </section>
                 </div>
               </div>
-            )}
+              {/* similar posts */}
+              <div className="sidebar">
+                {similarPosts?.length > 0 && (
+                  <div className="similar-posts">
+                    <h2 className="text-center">You may also like</h2>
+                    <div className="similar-posts-container">
+                      {similarPosts.map((post: any) => (
+                        <Link key={post._id} to={`/posts/${post.slug}`}>
+                          <Card
+                            _id={post._id}
+                            title={post.title}
+                            content={post.content}
+                            category={post.category?.title}
+                            image={post.image}
+                            author={`${post.author?.firstName} ${post.author?.lastName}`}
+                            authorImage={post.author?.image}
+                            commentsCount={post.commentsCount}
+                            date={new Date(post.createdAt).toLocaleDateString()}
+                          />
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         )
       )}
